92b1c28618f4e100342f678c1166e239a0a5a5ed
csturgeon <sturgeon.chris@gmail.com>
2022-06-08 11:16:00 +0100

    Win logic working

--- scripts.js
+++ scripts.js
@@ -12,16 +12,54 @@ const gameBrain = (() => {
       boardArr.push(num);
 
       if (gameBrain.turn === 'playerOne') {
+        player1.playerArr += num;
         var cell = document.getElementById(`${num}`);
         cell.innerText = "X";
         gameBrain.turn = 'playerTwo';
+        checkWinner(player1.playerArr);
+        updateTurnShow();
       } else {
+        player2.playerArr += num;
         var cell = document.getElementById(`${num}`);
         cell.innerText = "O";
+        checkWinner(player2.playerArr);
         gameBrain.turn = 'playerOne';
+        updateTurnShow();
       }
     };
   };
+
+  const checkWinner = function(playerArr) {
+    const winningCombos = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [7, 5, 3]
+    ];
+    var winnerArr = [];
+
+    for (let i = 0; i < winningCombos.length; i++) {
+      const winner = winningCombos[i].every(value => {
+        return playerArr.includes(value);
+      })
+      winnerArr.push(winner);
+    };
+
+    if (winnerArr.includes(true)) {
+      console.log('WE HAVE A WINNER!!!')
+    };
+
+    if (boardArr.length == 9) {
+      console.log('Draw!')
+    }
+
+
+  
+  }
 
   return {
     turn,
@@ -30,16 +68,16 @@ const gameBrain = (() => {
   };
 })();
 
-// displayController module
+// gameboard module
 
 const gameboard= (() => {
   const makeBoard = () => {
-    const container = document.getElementById('container');
+    const board = document.getElementById('board');
     for (let i = 1; i < 10; i++) {
       var cell = document.createElement('div');
       cell.classList.add('gridCell');
       cell.setAttribute('id', i);
-      cell.innerText = 'test';
+      cell.innerText = '';
       cell.value = i;
       cell.addEventListener('click', function() { gameBrain.assignSquare(this.value) });
       board.appendChild(cell);
@@ -53,7 +91,8 @@ const gameboard= (() => {
 
 const Player = function(name) {
   var playerArr = [];
-  return {name, playerArr};
+  var score = 0;
+  return {name, playerArr, score};
 }
 
 const player1 = Player('playerOne');
@@ -67,6 +106,21 @@ function updateTurnShow() {
 }
 
 updateTurnShow();
+
+const winningCombos = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 7],
+  [2, 5, 8],
+  [3, 8, 9],
+  [1, 5, 8],
+  [7, 5, 3]
+];
+
+
+
 
 
 
