775c7fbc8adc882d3a9db25d90d2e371c8588224
csturgeon <sturgeon.chris@gmail.com>
2022-06-09 15:32:14 +0100

    Complete JS refactoring

--- scripts.js
+++ scripts.js
@@ -1,38 +1,36 @@
-// Gameboard module
-
+// Gamelogic module
 const gameBrain = (() => {
-  var boardArr = [];
-  var turn = 'playerOne';
-  var active = true;
 
+  // Array for storing placed marker locations and set first player's turn. 
+  var markers = [];
+  var turn = 'playerOne';
+
+  // Assigns marker to square if not already occupied, then checks to see if this is a winning move, switching turns if not. 
+  // Receives num as grid square value through eventlistener. 
   const assignSquare = function(num) {
-    if (gameBrain.active) {
-      if (gameBrain.boardArr.includes(num)) {
+      if (gameBrain.markers.includes(num)) {
       } else {
-        boardArr.push(num);
-  
+        markers.push(num);
         if (gameBrain.turn === 'playerOne') {
-          player1.playerArr += num;
+          player1.markers += num;
           var cell = document.getElementById(`${num}`);
           cell.innerText = "X";
-          checkWinner(player1.playerArr);
+          checkWinner(player1.markers);
           gameBrain.turn = 'playerTwo';
         } else {
-          player2.playerArr += num;
+          player2.markers += num;
           var cell = document.getElementById(`${num}`);
           cell.innerText = "O";
-          checkWinner(player2.playerArr);
+          checkWinner(player2.markers);
           gameBrain.turn = 'playerOne';
-          
         };
-        gameboard.showTurn();
-      };
+        gameboard.updateTurn();
     }
   };
 
-  // Compare each player's placements to see if they match game-winning combinations.
-
-  const checkWinner = function(playerArr) {
+  // Checks if a player's markers array contains any game-winning combinations.
+  // If so, increments player's score and announces result. 
+  const checkWinner = function(playerMarkers) {
     const winningCombos = [
       [1, 2, 3],
       [4, 5, 6],
@@ -44,71 +42,56 @@ const gameBrain = (() => {
       [7, 5, 3]
     ];
     var winnerArr = [];
-
     for (let i = 0; i < winningCombos.length; i++) {
       const winner = winningCombos[i].every(value => {
-        return playerArr.includes(value);
+        return playerMarkers.includes(value);
       });
       winnerArr.push(winner);
     };
-
     if (winnerArr.includes(true) && gameBrain.turn == 'playerOne')  {
       setTimeout(function() {gameboard.declareResult(`${player1.displayName} wins`)}, 300);
       player1.score ++;
       gameboard.updateScore(player1.score, 'playerOneScore');
-      gameBrain.active = false;
-      gameboard.hideTurn();
     } else if (winnerArr.includes(true) && gameBrain.turn == 'playerTwo') {
       setTimeout(function() {gameboard.declareResult(`${player2.displayName} wins`)}, 300);
       player2.score ++;
       gameboard.updateScore(player2.score, 'playerTwoScore');
-      gameBrain.active = false;
-      gameboard.hideTurn();
-    } else if (gameBrain.boardArr.length == 9) {
+    } else if (gameBrain.markers.length == 9) {
       setTimeout(function() {gameboard.declareResult("It's a draw")}, 300);
-      gameBrain.active = false;
-      gameboard.hideTurn();
-    }
+    };
   };
-
-  return {
-    turn,
-    boardArr,
-    assignSquare,
-  };
+  return {turn, markers, assignSquare};
 })();
 
-// Gameboard module for displaying the board and scores. 
-
-// Display start intro modal
-
-const intro = function() {
-  document.getElementById('introModal').style.display = "flex";
-}
+// Gameboard module for creating board and updating UI.  
 
 const gameboard = (() => {
+
+  // Creates gameboard
   const makeBoard = () => {
     const board = document.getElementById('board');
     for (let i = 1; i < 10; i++) {
       var cell = document.createElement('div');
       cell.classList.add('gridCell');
       cell.setAttribute('id', i);
-      cell.innerText = '';
       cell.value = i;
       cell.addEventListener('click', function() { gameBrain.assignSquare(this.value) });
       board.appendChild(cell);
     };
   };
 
-  // Show whose turn it is.
+  // Displays introduction modal.
+  const intro = function() {
+    document.getElementById('introModal').style.display = "flex";
+  }
 
-  const showTurn = function() {
+  // Displays active player on sidebar.
+  const updateTurn = function() {
     var turnText = document.getElementById('turnText');
     var turnPic = document.getElementById('turnPic')
 
     if (gameBrain.turn == "playerOne") {
       turnText.innerText = `${player1.displayName}'s turn:`;
-      
       turnPic.innerText = "X";
     } else {
       turnText.innerText = `${player2.displayName}'s turn:`;
@@ -116,50 +99,41 @@ const gameboard = (() => {
     };
   };
 
-  // Hide next turn 
-
+  // Hides next turn on sidebar.
   const hideTurn = function() {
     document.getElementById('turn').style.opacity = '0';
   }
 
-  // Show next turn 
-
-  const displayTurn = function() {
+  // Shows next turn on sidebar. 
+  const showTurn = function() {
     document.getElementById('turn').style.opacity = '100';
   }
 
-  // Declare result once either player has won, or it's a draw.
-
+  // Declares result on modal once either player has won, or it's a draw.
   const declareResult = function(result) {
-
+    gameboard.hideTurn();
     document.getElementById('result').innerText = `${result}!`;
     document.getElementById('modal').style.display = 'flex';
   };
 
-  // Update the scoreboard.
-
+  // Updates scoreboard.
   const updateScore = function(score, player) {
     document.getElementById(`${player}`).innerText = `${score}`;
   }
 
-  // Start new game. 
-
+  // Starts new game for second round onwards, clearing all marker arrays.
   const newGame = function() {
     document.getElementById('modal').style.display = 'none';
-    gameBrain.active = true;
     document.getElementById('board').innerHTML = '';
-    document.getElementById('result').innerText = 'Result:'
-    gameBrain.boardArr.splice(0, gameBrain.boardArr.length);
-    player1.playerArr = [];
-    player2.playerArr = [];
+    gameBrain.markers.splice(0, gameBrain.markers.length);
+    player1.markers = [];
+    player2.markers = [];
     gameboard.makeBoard();
-    gameboard.displayTurn();
+    gameboard.showTurn();
   }
 
-  // Set player display names
-
+  // Sets player display names from intro modal if requested (defaults declare if not) updating UI elements after.  
   const setNames = function() {
-
     var p1 = document.getElementById('p1').value;
     var p2 = document.getElementById('p2').value;
     
@@ -179,21 +153,24 @@ const gameboard = (() => {
     document.getElementById('introModal').style.display = 'none';
     document.getElementById('turn').style.opacity = '100';
     document.getElementById('scores').style.opacity = '100';
-    gameboard.showTurn();
+    gameboard.updateTurn();
   }
 
-  // Declined new game
-
+  // Thanks user for playing if new game is declined. 
   const decline = function() {
-    document.getElementById('modal').style.display = 'none';
+    document.getElementById('result').innerText = "Thanks for playing!"; 
+    document.getElementById('result').style.marginTop = "25vh";
+    document.getElementById('question').style.display = "none";
+    document.getElementById('yes').style.display = "none";
+    document.getElementById('no').style.display = "none";
   }
 
   return {
     intro,
     makeBoard, 
-    showTurn, 
+    updateTurn, 
     hideTurn,
-    displayTurn,
+    showTurn,
     declareResult, 
     updateScore, 
     newGame,
@@ -203,24 +180,19 @@ const gameboard = (() => {
 })();
 
 
-// Player factory 
-
+// Player object factory 
 const Player = function(name) {
-  var playerArr = [];
+  var markers = [];
   var score = 0;
-
-  return {name, playerArr, score};
+  return {name, markers, score};
 }
 
+// On loading create player objects, board and display introduction. 
 var player1 = Player('playerOne', 'Player One');
 var player2 = Player('PlayerTwo', 'Player Two');
 
-
-
 gameboard.makeBoard();
-gameBrain.active = true;
 gameboard.intro();
-gameboard.showTurn();
 
 
 
